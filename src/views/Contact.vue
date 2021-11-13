37a10a4ed864e800c64714c5652edebe7e2213a6
teele <teele.tani@gmail.com>
2021-11-14 01:51:15 +0200

    Added view Contact

---
+++ src/views/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <div class="contact">
+      <h1>This is a contact us page</h1>
+    </div>
+  </main>
+
+</template>
+
+<script>
+export default {
+  name: "Contact"
+}
+</script>
+
+<style scoped>
+main {
+  margin-top: 110px;
+}
+
+.contact {
+  text-align: center;
+}
+</style>
