9f21cea5abe5bddcb96fa306a0fcbeacb8e84d14
teele <teele.tani@gmail.com>
2021-11-19 21:02:30 +0200

    Completed the Contact Us view

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,26 +1,46 @@
 <!-- TEMPLATE -->
 <template>
   <main>
-    <div class="contact">
-      <h1>This is a contact us page</h1>
-    </div>
+    <section id="content">
+      <!-- beginning of content -->
+      <div id="content-flow">
+        <ContactForm/>
+        <ContactInfo/>
+      </div>
+    </section>
   </main>
 </template>
 
 <!-- SCRIPT -->
 <script>
+import ContactForm from "../components/ContactForm";
+import ContactInfo from "../components/ContactInfo"
+
 export default {
-  name: "Contact"
+  name: "Contact",
+  components: {
+    ContactForm,
+    ContactInfo
+  }
 }
 </script>
 
 <!-- STYLE -->
 <style scoped>
 main {
-  margin-top: 110px;
+  margin-top: 70px;
 }
 
-.contact {
-  text-align: center;
+#content {
+  display: flex;
+  flex-direction: column;
+  width: 65%;
+  min-height: calc(100vh - 70px);
+  margin: auto;
+  background-color: #ffffff;
+}
+
+#content #content-flow {
+  padding: 40px 0 20px 0;
 }
 </style>
