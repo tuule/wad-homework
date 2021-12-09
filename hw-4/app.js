1ef4f3ff31db6b59f5569fefafaed6feaabd9741
tuule <tuule.tani@gmail.com>
2021-12-09 14:07:59 +0200

    Adding posts and viewing one post

--- hw-4/app.js
+++ hw-4/app.js
@@ -8,17 +8,18 @@ const app = express();
 // register the ejs view engine
 app.set('view engine', 'ejs');
 
-// listen for requests on port 3000
-app.listen(3000);
-
 // use static files
 app.use(express.static('public'))
-
+//without this middleware, we cannot use data submitted by forms
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
     next();
 });
+
+// listen for requests on port 3000
+app.listen(3000);
 
 /* app.get() is used to respond to Get requests: */
 app.get('/', async(req, res) => {
@@ -33,13 +34,35 @@ app.get('/', async(req, res) => {
     }
 });
 
-
-app.get('/singlepost', (req, res) => {
-    res.render('singlepost');
+app.get('/posts/:id', async(req, res) => {
+    try {
+        console.log("get a single post request has arrived");
+        const { id } = req.params;
+        const postData = await pool.query(
+            "SELECT * FROM posts WHERE post_id = $1", [id]
+        );
+        res.render('singlepost', { post: postData.rows[0], helper: functions });
+    } catch (err) {
+        console.error(err.message);
+    }
 });
 
 app.get('/addnewpost', (req, res) => {
     res.render('addnewpost');
+});
+
+app.post('/posts', async(req, res) => {
+    try {
+        const post = req.body;
+        console.log(post);
+        const newpost = await pool.query(
+            "INSERT INTO posts(post_date, post_title, post_text, post_image_url, number_of_likes, author_avatar_url, has_been_liked) values (CURRENT_TIMESTAMP, $1, $2, $3, 0, 'https://kodu.ut.ee/~a71486/user-3.png', false) RETURNING *",
+            [post.post_title, post.post_text, post.post_image_url]
+    );
+        res.redirect('/');
+    } catch (err) {
+        console.error(err.message)
+    }
 });
 
 app.get('/contact', (req, res) => {
