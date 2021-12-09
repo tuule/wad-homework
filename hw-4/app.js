6d4caadc30d89792783e90605bd7a0c9da3dd1b3
tuule <tuule.tani@gmail.com>
2021-12-09 21:39:43 +0200

    Added functionality to delete single post, fixed minor style issues

--- hw-4/app.js
+++ hw-4/app.js
@@ -10,9 +10,9 @@ app.set('view engine', 'ejs');
 
 // use static files
 app.use(express.static('public'))
-//without this middleware, we cannot use data submitted by forms
+// without this middleware, we cannot use data submitted by forms
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json()); // for testing
 
 app.use((req, res, next) => {
     next();
@@ -24,7 +24,7 @@ app.listen(3000);
 /* app.get() is used to respond to Get requests: */
 app.get('/', async(req, res) => {
     try {
-        console.log("get posts request has arrived");
+        console.log("Get request to \'/\'");
         const postData = await pool.query(
             "SELECT * FROM posts ORDER BY post_date DESC"
         );
@@ -34,19 +34,23 @@ app.get('/', async(req, res) => {
     }
 });
 
+app.get('/posts', (req, res) => {
+    console.log("Get request to \'/posts\', redirected to \'/\'");
+    res.redirect('/');
+});
+
 app.get('/posts/:id', async(req, res) => {
     try {
-        console.log("get a single post request has arrived");
-        const { id } = req.params;
-        console.log(req.params);
+        const id = req.params.id; // or you could write it like this -> const { id } = req.params;
+        console.log("Get request to \'/posts/id/" + id + "\'");
         const postData = await pool.query(
             "SELECT * FROM posts WHERE post_id = $1", [id]
         );
-        console.log(postData.rows[0])
         if (postData.rows.length > 0) {
             res.render('singlepost', { post: postData.rows[0], helper: functions });
         }
         else {
+            console.log("No post with this post_id in database, redirected to \'/404\'");
             res.redirect('/404');
         }
     } catch (err) {
@@ -55,27 +59,46 @@ app.get('/posts/:id', async(req, res) => {
 });
 
 app.get('/addnewpost', (req, res) => {
+    console.log("Get request to \'/addnewpost\'");
     res.render('addnewpost');
 });
 
-app.post('/posts', async(req, res) => {
+app.post('/add', async(req, res) => {
     try {
         const post = req.body;
+        console.log("Post request to \'/add\'");
         console.log(post);
         const newpost = await pool.query(
             "INSERT INTO posts(post_date, post_title, post_text, post_image_url, number_of_likes, author_avatar_url, has_been_liked) values (CURRENT_TIMESTAMP, $1, $2, $3, 0, 'https://kodu.ut.ee/~a71486/user-3.png', false) RETURNING *",
             [post.post_title, post.post_text, post.post_image_url]
     );
+        console.log("After post request redirect to \'/\'");
         res.redirect('/');
     } catch (err) {
         console.error(err.message)
     }
 });
 
+app.delete('/posts/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+        console.log("Delete request to \'/posts/" + id + "\'");
+        const deletepost = await pool.query(
+            "DELETE FROM posts WHERE post_id = $1", [id]
+        );
+        console.log("After delete request redirect to \'/\'");
+        res.redirect('/');
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 app.get('/contact', (req, res) => {
+    console.log("Get request to \'/contact\'");
     res.render('contact');
 });
 
 app.use((req, res) => {
+    console.log("Well done, you have reached the 404 page");
     res.status(404).render('404', { message: 'Uh-oh! The page you were looking for does not exist.', title: 'Page Not Found'});
 });
